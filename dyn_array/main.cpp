aa386c09dbe1b0da7ffa000dea99bc2dd2878666
Vitaly Kravchenko <vitalyx@gmail.com>
2020-08-13 08:53:18 +0100

    Realloc fixes. Use optional.

--- dyn_array/main.cpp
+++ dyn_array/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <cmath>
 #include <iostream>
 #include <vector>
 #include <optional>
@@ -13,26 +14,27 @@ public:
         std::free(_storage);
     }
 
-    void push(T value)
+    bool push(T value)
     {
         if (_length >= _capacity) {
             if (_capacity > 0) {
-                _capacity *= 2;
-                if (std::realloc(_storage, _capacity) == nullptr) {
-                    std::cout << "Array - Couldn't reallocate memory." << std::endl;
-                    std::exit(1);
+                auto new_capacity = _capacity * 2;
+                if (std::realloc(_storage, new_capacity * sizeof(T)) == nullptr) {
+                    return false;
                 }
+                _capacity *= new_capacity;
             } else {
-                _storage = static_cast<T*>(std::malloc(sizeof(T)));
+                auto new_capacity = 1;
+                _storage = static_cast<T*>(std::malloc(new_capacity * sizeof(T)));
                 if (_storage == nullptr) {
-                    std::cout << "Array - Couldn't allocated memory." << std::endl;
-                    std::exit(1);
+                    return false;
                 }
-                _capacity = 1;
+                _capacity = new_capacity;
             }
         }
         _storage[_length] = value;
         _length += 1;
+        return true;
     }
 
     std::optional<T> pop();
@@ -53,7 +55,14 @@ public:
         return {};
     }
 
-    bool compact();
+    bool compact() {
+        auto new_capacity = std::pow(2, std::ceil(std::log2(_capacity)));
+        if (std::realloc(_storage, new_capacity * sizeof(T)) == nullptr) {
+            return false;
+        }
+        _capacity = new_capacity;
+        return true;
+    }
 
     std::optional<T> operator[](size_t index)
     {
@@ -65,7 +74,7 @@ public:
 
     inline size_t length() { return _length; }
 
-    inline size_t capacity() { return _capacity; }
+    inline size_t capacity() { return _capacity; } // zero or a number that is a power of two
 
 private:
     T* _storage;
@@ -76,6 +85,7 @@ private:
 int main(int, char**)
 {
     Array<int> array;
+    std::cout << "Last element is over " << array.last().value_or(9000) << std::endl;
     array.push(5);
     array.push(7);
     array.push(9);
@@ -88,7 +98,7 @@ int main(int, char**)
     }
     auto last = array.last();
     if (last) {
-        std::cout << "Last: " << last.value() << std::endl;
+        std::cout << "Last: " << *last << std::endl;
     }
 
     std::cout << "Length: " << array.length() << std::endl;
