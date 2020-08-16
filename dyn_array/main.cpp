72cee82a3a8ed2f6027a7b43ea5aa74840544fcb
Vitaly Kravchenko <vitalyx@gmail.com>
2020-08-16 19:15:18 +0100

    Format code.

--- dyn_array/main.cpp
+++ dyn_array/main.cpp
@@ -1,12 +1,12 @@
+#include <assert.h>
 #include <cmath>
 #include <cstdlib>
+#include <execinfo.h>
 #include <iostream>
 #include <optional>
-#include <vector>
-#include <assert.h>
-#include <execinfo.h>
 #include <signal.h>
 #include <unistd.h>
+#include <vector>
 
 static int FooId = 0;
 struct Foo {
@@ -186,16 +186,16 @@ void pop_value(Array<int>& array) {
 }
 
 void handler(int sig) {
-  void *array[10];
-  size_t size;
+    void* array[10];
+    size_t size;
 
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 20);
+    // get void*'s for all entries on the stack
+    size = backtrace(array, 20);
 
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
+    // print out all the frames to stderr
+    fprintf(stderr, "Error: signal %d:\n", sig);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+    exit(1);
 }
 
 int main(int, char**) {
